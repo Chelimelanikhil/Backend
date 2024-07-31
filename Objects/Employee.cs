868ffc79430a2a729f0e73b47cdcc10dc6a40f7a
brio support <brio support@DESKTOP-HMU99NO>
2024-07-31 16:48:38 +0530

    commit

---
+++ Objects/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace AppBackend.Objects
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public int? TenantID { get; set; }
+        public int? DepartmentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateOfJoining { get; set; }
+        public string WeekOff1 { get; set; }
+        public string WeekOff2 { get; set; }
+        public string JobTitle { get; set; }
+        public decimal? Salary { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public string Status { get; set; }
+        public int BranchId {  get; set; }   
+        public int ShiftId { get; set; }
+    }
+
+}
